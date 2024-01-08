f665a06757a4bd07590fbb4a2d88fadd30f79c34
PoAn Yang <poan.yang@suse.com>
2024-01-08 13:42:54 +0800

    fix: ignore pvc checking if deletionpolicy is empty
    Signed-off-by: PoAn Yang <poan.yang@suse.com>

--- pkg/webhook/resources/virtualmachinerestore/validator.go
+++ pkg/webhook/resources/virtualmachinerestore/validator.go
@@ -206,7 +206,7 @@ func (v *restoreValidator) checkBackup(vmRestore *v1beta1.VirtualMachineRestore,
 		return err
 	}
 
-	if vmRestore.Spec.DeletionPolicy == v1beta1.VirtualMachineRestoreRetain {
+	if (vmRestore.Spec.DeletionPolicy == v1beta1.VirtualMachineRestoreRetain) || (vmRestore.Spec.DeletionPolicy == "") {
 		return nil
 	}
 
